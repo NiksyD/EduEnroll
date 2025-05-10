fd5c55311b4518379dd620119c6add8e42bac4eb
NiksyD <nikoandreimanuelpepito@gmail.com>
2025-05-10 21:26:04 +0800

    Finished Product

--- types/index.ts
+++
@@ -1,9 +0,0 @@
-import { type EnrollmentStatus } from "@/context/enrollment-context"
-
-export interface Enrollment {
-  id: string;
-  studentName: string;
-  status: EnrollmentStatus;
-  grade: string | null;
-  remarks: string | null;
-} 
